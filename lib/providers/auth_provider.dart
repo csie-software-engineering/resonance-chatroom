48c5341936de836284328ff67f627fda7a33bd2b
sgoina <sgoina2003@gmail.com>
2023-11-19 22:40:39 +0800

    anonymous & google _sign_in

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthProviders extends ChangeNotifier {
+  final FirebaseAuth firebaseAuth;
+  final SharedPreferences prefs;
+
+  AuthProviders({
+    required this.firebaseAuth,
+    required this.prefs,
+  });
+
+  signInWithAnonymous() async {
+    try {
+      final userCredential =
+      await FirebaseAuth.instance.signInAnonymously();
+      print("Signed in with temporary account.");
+      print(userCredential.user?.uid);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "operation-not-allowed":
+          print("Anonymous auth hasn't been enabled for this project.");
+          break;
+        default:
+          print("Unknown error.");
+      }
+    }
+  }
+  signInWithGoogle() async {
+    GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    UserCredential userCredential =
+    await FirebaseAuth.instance.signInWithCredential(credential);
+    print(userCredential.user?.displayName);
+    print(userCredential.user?.email);
+
+  }
+  logout() async {
+    final GoogleSignIn googleSign = GoogleSignIn();
+    await googleSign.signOut();
+  }
+}
--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export 'src/chat_provider.dart';
+=======
+export 'auth_provider.dart';
+>>>>>>> 3328a06 (anonymous & google _sign_in)

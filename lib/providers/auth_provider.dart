c8f7f3b2d2fc06e41b413b82b389d28267d07ecb
sgoina <sgoina2003@gmail.com>
2023-11-19 22:41:02 +0800

    login_provider

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class AuthProviders extends ChangeNotifier {
   final FirebaseAuth firebaseAuth;
@@ -40,6 +41,22 @@ class AuthProviders extends ChangeNotifier {
     print(userCredential.user?.displayName);
     print(userCredential.user?.email);
 
+  }
+  signInWithFacebook() async {
+    // Trigger the sign-in flow
+    final LoginResult loginResult = await FacebookAuth.instance.login(
+        permissions: ['email', 'public_profile', 'user_birthday']
+    );
+
+    // Create a credential from the access token
+    final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+    final userData = await FacebookAuth.instance.getUserData();
+
+    String userEmail = userData['email'];
+    print(userEmail);
+    // Once signed in, return the UserCredential
+    await FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
   }
   logout() async {
     final GoogleSignIn googleSign = GoogleSignIn();

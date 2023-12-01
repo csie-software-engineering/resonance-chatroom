9836a182cabddb2b10552ffa56d33776ff3ef5ea
Staravi <dadninele@gmail.com>
2023-12-01 15:33:16 +0800

    feature: 新增日程、活動 Card、配對按鈕變化
    1. 日程：組織方將活動日程登記在這邊，每天的行程。
    2. 活動：描述日程中提到的活動，以及活動懶人包。
    
    issue #17

--- lib/pages/src/user_activity_main_page.dart
+++ lib/pages/src/user_activity_main_page.dart
@@ -8,6 +8,11 @@ class UserActivityMainPage extends StatefulWidget {
 }
 
 class _UserActivityMainPageState extends State<UserActivityMainPage> {
+  FloatingActionButtonLocation buttonPosition =
+      FloatingActionButtonLocation.centerFloat;
+  Color buttonColor = Colors.yellow;
+  bool startMatching = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,37 +20,113 @@ class _UserActivityMainPageState extends State<UserActivityMainPage> {
         backgroundColor: Theme.of(context).colorScheme.surface,
         elevation: 0,
         leading: const BackButton(),
+        // leadingWidth: 20,
         title: Center(
-            // child: DecoratedBox(
-            //   decoration: BoxDecoration(
-            //     color: Theme.of(context).colorScheme.background,
-            //     borderRadius: const BorderRadius.all(Radius.circular(16)),
-            //   ),
-              child: SizedBox(
-                child: Align(
-                  alignment: Alignment.center, // <-- 這裡設定text的對齊方式
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(16)),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 20),
-                      color: Theme.of(context).colorScheme.background,
-                      child: Text(
-                        '三峽科技人 CTM',
-                      ),
-                    ),
-                  ),
+          child: Align(
+            alignment: Alignment(0, 0), // <-- 這裡設定text的對齊方式
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(16)),
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 2, horizontal: 20.0),
+                color: Theme.of(context).colorScheme.background,
+                child: Text(
+                  '三峽科技人 CTM',
                 ),
               ),
             ),
           ),
-        body: Container(
-          color: Colors.red,
         ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Text("配對"),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16.0),
+            child: Image.network(
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq6O3JNBylobYryHbyakwOtJ_WX8lHitEjQg&usqp=CAU",
+            ),
+          ),
+          CardExample(title: "日程"),
+          CardExample(title: "活動"),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: buttonColor,
+        onPressed: () {
+          setState(() {
+            if (!startMatching) {
+              buttonPosition = FloatingActionButtonLocation.endFloat;
+              startMatching = true;
+              buttonColor = Theme.of(context).colorScheme.primary;
+            } else {
+              buttonPosition = FloatingActionButtonLocation.centerFloat;
+              startMatching = false;
+              buttonColor = Theme.of(context).colorScheme.secondary;
+            }
+          });
+        },
+        child: Text(
+          "配對",
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: buttonPosition,
     );
   }
 }
+
+class CardExample extends StatelessWidget {
+  const CardExample({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Card(
+          clipBehavior: Clip.hardEdge,
+          elevation: 2,
+          child: InkWell(
+            onTap: () {},
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.album),
+                  title: Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text('BUY TICKETS'),
+                      onPressed: () {/* ... */},
+                    ),
+                    const SizedBox(width: 8),
+                    TextButton(
+                      child: const Text('LISTEN'),
+                      onPressed: () {/* ... */},
+                    ),
+                    const SizedBox(width: 8),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+

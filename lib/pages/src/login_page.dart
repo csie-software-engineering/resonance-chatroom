0aae6923b5b9596aa0ebd951a95a44053f872f9d
sgoina <sgoina2003@gmail.com>
2023-12-07 03:59:48 +0800

    登入前後端串接

--- lib/pages/src/login_page.dart
+++ lib/pages/src/login_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:resonance_chatroom/pages/pages.dart';
+import '../../providers/providers.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatelessWidget{
   const LoginPage({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context){
+    late final AuthProviders authProvider = context.read<AuthProviders>();
     return Scaffold(
         body: Container(
             alignment: Alignment.center,
@@ -15,7 +20,9 @@ class LoginPage extends StatelessWidget{
                 SizedBox(
                     width: 200.0,
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () async {
+                        await authProvider.signInWithGoogle();
+                      },
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -28,7 +35,9 @@ class LoginPage extends StatelessWidget{
                 SizedBox(
                     width: 200.0,
                     child: ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () async {
+                          await authProvider.signInWithFacebook();
+                        },
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
@@ -42,7 +51,9 @@ class LoginPage extends StatelessWidget{
                 SizedBox(
                     width: 200.0,
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () async {
+                        await authProvider.signInWithAnonymous();
+                      },
                       child: Text('Login as guest'),
                     )
                 )

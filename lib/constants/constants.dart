999f0f0a2221bafef6700fdfb566fdc60f526c4c
garyellow <gary20011110@gmail.com>
2023-12-08 12:18:45 +0800

    feat: 添加 user 的常數

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,4 @@
 export 'src/app_constants.dart';
 export 'src/chat_constants.dart';
 export 'src/firestore_constants.dart';
-export 'src/user_constants.dart';
+export 'src/firestore/fs_user_constants.dart';

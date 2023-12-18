4cd2f0a2f6fa214c212483e8cb06f149b23a6c8e
garyellow <gary20011110@gmail.com>
2023-12-18 20:39:57 +0800

    chore: util

--- lib/utils/src/image.dart
+++ lib/utils/src/image.dart
@@ -1,6 +1,13 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:image_picker/image_picker.dart';
+
+Future<String?> pickImageToBase64() async {
+  final xFile = (await ImagePicker().pickImage(source: ImageSource.gallery));
+  if (xFile == null) return null;
+  return imagePathToBase64(xFile.path);
+}
 
 Future<String?> imagePathToBase64(String imagePath) async {
   var imageFile = File(imagePath);
@@ -8,9 +15,9 @@ Future<String?> imagePathToBase64(String imagePath) async {
 }
 
 Future<String?> imageFileToBase64(File imageFile) async {
-  if (!imageFile.existsSync()) return null;
+  if (!await imageFile.exists()) return null;
   var bytes = await imageFile.readAsBytes();
-  return base64Encode(bytes);
+  return imageToBase64(bytes);
 }
 
 String imageToBase64(Uint8List image) {

5ac4f395770113343b87ad1b8a715c802dc77213
garyellow <gary20011110@gmail.com>
2023-12-18 19:54:39 +0800

    refactor: Remove unused import and update user provider

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -3,5 +3,4 @@ export 'src/chat_message.dart';
 export 'src/chat_queue_node.dart';
 export 'src/room.dart';
 export 'src/user.dart';
-export 'src/firestore/fs_user.dart';
 export 'src/activities.dart';

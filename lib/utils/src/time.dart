079d6eba2318fd3d2beac6988a9c956168a0c193
garyellow <gary20011110@gmail.com>
2023-12-18 20:03:32 +0800

    feat: add base64 and time extensions

--- lib/utils/src/time.dart
+++ lib/utils/src/time.dart
@@ -1,11 +1,7 @@
-extension StringExtension on String {
-  DateTime toEpochTime() {
-    return DateTime.fromMillisecondsSinceEpoch(int.parse(this));
-  }
+extension DateTimeExtension on DateTime {
+  String toEpochString() => millisecondsSinceEpoch.toString();
 }
 
-extension DateTimeExtension on DateTime {
-  String toEpochString() {
-    return millisecondsSinceEpoch.toString();
-  }
+extension StringExtension on String {
+  DateTime toEpochTime() => DateTime.fromMillisecondsSinceEpoch(int.parse(this));
 }

5b3386c1791ea2cb1c4d989e658d2d9a167312bd
garyellow <gary20011110@gmail.com>
2023-12-22 15:06:25 +0800

    Remove InitPage and Add WelcomePage

--- lib/pages/routes.dart
+++ lib/pages/routes.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'src/chat.dart';
 import 'src/user_activity_main_page.dart';
 import 'src/host_activity_set_page.dart';
-import 'src/init.dart';
 import 'src/login_page.dart';
 import 'src/host_activity_tag_page.dart';
 import 'src/host_activity_topic_page.dart';
@@ -14,7 +13,6 @@ import 'src/personal_setting.dart';
 
 export 'src/chat.dart';
 export 'src/host_activity_set_page.dart';
-export 'src/init.dart';
 export 'src/login_page.dart';
 export 'src/host_activity_tag_page.dart';
 export 'src/host_activity_topic_page.dart';
@@ -24,7 +22,6 @@ export 'src/main_page.dart';
 export 'src/personal_setting.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
-  InitPage.routeName: (_) => const InitPage(),
   HostActivitySetPage.routeName: (_) => const HostActivitySetPage(),
   LoginPage.routeName: (_) => const LoginPage(),
   ChatPage.routeName: (_) => const ChatPage(),

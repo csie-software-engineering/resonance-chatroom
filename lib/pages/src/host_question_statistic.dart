2b07e68250611f09a05f1cb4f2d01975b6718b1d
garyellow <gary20011110@gmail.com>
2023-12-24 03:48:57 +0800

    feat: Add host question statistic page and add dynamic chart generation

--- lib/pages/src/host_question_statistic.dart
+++ lib/pages/src/host_question_statistic.dart
@@ -1,92 +1,107 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' hide TextStyle;
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:provider/provider.dart';
+
+import '../../models/models.dart';
+import '../../providers/providers.dart';
+
+class HostQuestionStatisticPageArguments {
+  final String activityId;
+
+  const HostQuestionStatisticPageArguments(this.activityId);
+}
 
 class HostQuestionStatisticPage extends StatelessWidget {
   const HostQuestionStatisticPage({super.key});
 
-  static const routeName = '/horizontal_bar_chart';
+  static const routeName = '/host_question_statistic';
 
   @override
   Widget build(BuildContext context) {
-    final seriesList = _createSampleData();
-    return BarChart(
-      seriesList,
-      animate: true,
-      vertical: false,
-    );
-  }
-
-  static List<Series<OrdinalSales, String>> _createSampleData() {
-    final random = Random();
-    final data = [
-      OrdinalSales('項目1', random.nextInt(101)),
-      OrdinalSales('項目2', random.nextInt(101)),
-      OrdinalSales('項目3', random.nextInt(101)),
-      OrdinalSales('項目4', random.nextInt(101)),
-    ];
-
-    return [
-      Series<OrdinalSales, String>(
-        id: '銷售量',
-        domainFn: (OrdinalSales sales, _) => sales.year,
-        measureFn: (OrdinalSales sales, _) => sales.sales,
-        data: data,
-      )
-    ];
-  }
-}
-
-class OrdinalSales {
-  final String year;
-  final int sales;
-
-  OrdinalSales(this.year, this.sales);
-}
-
-class CombinedBarChartsPage extends StatelessWidget {
-  const CombinedBarChartsPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments
+        as HostQuestionStatisticPageArguments;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('統計圖'),
+        title: Text(
+          '問卷統計',
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: List.generate(10, (index) {
-            int chartIndex = index + 1;
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Text(
-                    '統計圖 $chartIndex', // 從這裡開始添加標題（從1開始）
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  const SizedBox(
-                    height: 200, // 根據需要調整高度
-                    child: HostQuestionStatisticPage(),
-                  ),
-                  const Divider(
-                    // 在每個統計圖之後添加一個Divider
-                    color: Colors.grey,
-                    thickness: 1.0,
-                    height: 20,
-                    indent: 20,
-                    endIndent: 20,
-                  ),
-                ],
-              ),
-            );
-          }),
+      body: Container(
+        color: Theme.of(context).colorScheme.background,
+        child: SingleChildScrollView(
+          child: FutureBuilder<List<Question>>(
+            future:
+                context.read<ActivityProvider>().getAllQuestions(args.activityId),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              final questions = snapshot.data as List<Question>;
+              return ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: questions.length,
+                itemBuilder: (context, index) {
+                  final question = questions[index];
+                  return Column(
+                    children: [
+                      Divider(
+                        height: MediaQuery.of(context).size.height * 0.03,
+                        color: index == 0 ? Colors.transparent : Colors.grey,
+                      ),
+                      Text(
+                        question.questionName,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      FutureBuilder<List<int>>(
+                        future: context.read<QuestionProvider>().getReplyResult(
+                              args.activityId,
+                              question.uid,
+                            ),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          }
+
+                          final result = snapshot.data!;
+                          final seriesList = [
+                            charts.Series<int, String>(
+                              id: question.uid,
+                              domainFn: (_, index) => question.choices[index!],
+                              measureFn: (int value, _) => value,
+                              data: result,
+                            )
+                          ];
+                          return SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.2,
+                            child: charts.BarChart(
+                              seriesList,
+                              animate: true,
+                              vertical: false,
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
         ),
       ),
     );
